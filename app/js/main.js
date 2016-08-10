01a97c3728b859cdace14826bd160515574f21a5
afeya24 <afeya24@gmail.com>
2016-08-10 14:40:34 +0300

    nerly destope version

--- app/js/main.js
+++ app/js/main.js
@@ -10,11 +10,28 @@
     slideSpeed : 300,
     paginationSpeed : 400,
     singleItem: true,
+    pagination: false,
     itemsCustom: false
   }
-  $("#owl-slideOne").owlCarousel(owlBaseSettings);
+  var owlBaseSettingsCarusel = {
+    navigation : true, // Show next and prev buttons
+    slideSpeed : 300,
+    paginationSpeed : 400,
+    singleItem: false,
+    pagination: false,
+    items : 3,
+      itemsDesktop : [1100,2],
+      itemsDesktopSmall : [979,2],
+      itemsTablet: [729,1],
+      itemsMobile : false
+    // itemsCustom: false
+  }
 
+  $("#owl-slideOne").owlCarousel(owlBaseSettings);
   $("#owl-slideTwo").owlCarousel(owlBaseSettings);
 
+
+  $("#owl-product").owlCarousel(owlBaseSettingsCarusel);
+  $("#owl-recept").owlCarousel(owlBaseSettingsCarusel);
 
 })(window);
